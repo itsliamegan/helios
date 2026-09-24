f1a254f43a8efa9a27168deab750f4be7f7d0a26
Liam Egan <liam@liamegan.com>
2026-09-24 10:45:59 -0400

    Collect database errors in database/error.py

--- src/helios/database/__init__.py
+++ src/helios/database/__init__.py
@@ -1,12 +1,12 @@
 from .attribute import attr as attr
 from .config import Config as Config
+from .error import DatabaseBusy as DatabaseBusy
+from .error import DatabaseError as DatabaseError
 from .error import ModelError as ModelError
+from .error import NotFoundError as NotFoundError
 from .model import Model as Model
 from .provider import Provider as Provider
 from .query import Query as Query
-from .sqlite import DatabaseBusy as DatabaseBusy
-from .sqlite import DatabaseError as DatabaseError
-from .store import NotFoundError as NotFoundError
 from .store import Store as Store
 from .types import Scalar as Scalar
 from .types import Type as Type
