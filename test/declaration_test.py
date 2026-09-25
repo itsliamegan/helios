9f94ed53eba1b085b7b2ae596881b3d707100c71
Liam Egan <liam@liamegan.com>
2026-09-25 15:01:12 -0400

    Add a check that a class inherits from a single base

--- test/declaration_test.py
+++ test/declaration_test.py
@@ -7,6 +7,7 @@ from helios.declaration import (
 	DeclarationError,
 	MISSING,
 	check_init_keywords,
+	check_single_base,
 	declarations,
 	split_nullable,
 )
@@ -122,3 +123,38 @@ def test_rejects_missing_init_keywords():
 		check_init_keywords(Post, "attributes", [], ["title", "body"], ["title"])
 
 	assert_eq(str(raised.exception), "Post is missing attributes: title")
+
+
+def test_accepts_a_single_base():
+	class Record:
+		pass
+
+	class Post(Record):
+		pass
+
+	check_single_base(Post, Record, ExampleError)
+
+
+def test_rejects_other_bases():
+	class Record:
+		pass
+
+	class Content(Record):
+		pass
+
+	class Post(Content):
+		pass
+
+	class Timestamped:
+		pass
+
+	class Note(Record, Timestamped):
+		pass
+
+	with assert_raises(ExampleError) as subclass_raised:
+		check_single_base(Post, Record, ExampleError)
+	with assert_raises(ExampleError) as mixin_raised:
+		check_single_base(Note, Record, ExampleError)
+
+	assert_eq(str(subclass_raised.exception), "Post must inherit only from Record")
+	assert_eq(str(mixin_raised.exception), "Note must inherit only from Record")
